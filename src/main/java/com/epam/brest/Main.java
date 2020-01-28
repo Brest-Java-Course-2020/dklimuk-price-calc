0ae1202584d1285dd38f11c25b7981e6a250c1a3
MrKlimuk <mr.d.klimuk@gmail.com>
2020-01-28 18:24:30 +0300

    First commit

---
+++ src/main/java/com/epam/brest/Main.java
@@ -0,0 +1,13 @@
+package com.epam.brest;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+
+    }
+
+}
